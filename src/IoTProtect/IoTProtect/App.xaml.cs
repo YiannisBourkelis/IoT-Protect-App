8d2b67f299850e6cb267c7a112510aebf353fc59
Yiannis Bourkelis <yiannis1234@gmail.com>
2020-05-03 03:55:37 +0300

    Initial commit - default xamarin forms tabbed project

---
+++ src/IoTProtect/IoTProtect/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using IoTProtect.Services;
+using IoTProtect.Views;
+
+namespace IoTProtect
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/IoTProtect/IoTProtect/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IoTProtect.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
